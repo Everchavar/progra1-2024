1a489f61903aad9ded4e4cb856b990814081516a
Everchavar <everchavarria07@gmail.com>
2024-08-11 19:25:02 -0600

    clasificacion de edades

---
+++ condicional_de_edad/condicional_de_edad/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace condicional_de_edad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //condicional para la edad de una persona
+            Console.Write("Edad: ");
+            int edadt = int.Parse(Console.ReadLine());
+            if (edadt >= 18)
+            {
+                Console.WriteLine("Bienvenido a la vida adulta");
+            }
+            else
+            { 
+                Console.WriteLine("disfrita mientas puedas");
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
