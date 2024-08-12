ae00ee5108000828bda692fb4ff23e6c0370531a
Everchavar <everchavarria07@gmail.com>
2024-08-11 19:50:27 -0600

    condicional Extendido y bucle

--- condicional_de_edad/condicional_de_edad/Program.cs
+++ condicional_de_edad/condicional_de_edad/Program.cs
@@ -11,20 +11,40 @@ namespace condicional_de_edad
         static void Main(string[] args)
         {
             //condicional para la edad de una persona
-            Console.Write("Edad: ");
-            int edadt = int.Parse(Console.ReadLine());
-            if (edadt >= 18)
-            {
-                Console.WriteLine("Bienvenido a la vida adulta");
-            }
-            else
-            { 
-                Console.WriteLine("disfrita mientas puedas");
+            string seguir = "g";
+            while (seguir == "g") {
+
+                Console.Write("Edad: ");
+                int edadt = int.Parse(Console.ReadLine());
+                if (edadt< 0)
+                {
+                    Console.WriteLine("edad incorrecta");
+                }
+                else if (edadt<=2)
+                {        
+                    Console.WriteLine("eres un bebe");
+
+                } else if (edadt<12)
+                    {
+                    Console.WriteLine("eres un niño");
+                }else if (edadt<18)
+                {
+                    Console.WriteLine("eres un adolescente");
+                    
+                }else if (edadt <= 64)
+                {
+                    Console.WriteLine("Bienbenido al mundo de las responsabilidades");
+                }else if (edadt <=80)
+                {
+                    Console.WriteLine("eres un adulto mayor");
+                }else  {
+                    Console.WriteLine("Larga vida");
+                }
+                Console.Write("Desea seguir presione g, si no cualquier tecla.\n");
+                seguir = Console.ReadLine();
 
             }
-
-
-            Console.ReadLine();
+            
         }
     }
 }
