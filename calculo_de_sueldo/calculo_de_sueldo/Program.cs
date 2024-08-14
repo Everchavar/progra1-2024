a38d2482b05add274bb3f056c6e0632e3003383d
Everchavar <everchavarria07@gmail.com>
2024-08-14 10:33:57 -0600

    determinar si el numero es primo

--- calculo_de_sueldo/calculo_de_sueldo/Program.cs
+++ calculo_de_sueldo/calculo_de_sueldo/Program.cs
@@ -8,21 +8,33 @@ namespace calculo_de_sueldo
 {
     class Program
     {
-       
-            static void Main(string[] args)
-            {
-            //determinar si un numero es par o impar
-            int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
-            foreach(int num in serie)
-            {
-                Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "par" : "impar");
-            }
-            
 
-            
-             Console.ReadLine();
+        static void Main(string[] args)
+        {
+            //determinar si un numero es primo
+            int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
+            foreach (int num in serie)
+            {
+
+                int i = 1,
+                    n = 0;
+
+                while (i <= num && n <= 2)
+                {
+                    if (num % i == 0)
+                    {
+                        n++;
+                    }
+                    i++;
+                }
+
+
+                Console.WriteLine("El numero {0} {1}", num, n <= 2 ? "Es primo" : "No es primo");
             }
+                Console.ReadLine();
+            
         }
     }
+}
 
 
