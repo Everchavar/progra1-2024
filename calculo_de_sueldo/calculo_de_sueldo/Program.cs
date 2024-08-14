fb44bd5d4ac24fec9df4412e06e3ce548a00756c
Everchavar <everchavarria07@gmail.com>
2024-08-14 09:46:06 -0600

    secuencia fibonacci

--- calculo_de_sueldo/calculo_de_sueldo/Program.cs
+++ calculo_de_sueldo/calculo_de_sueldo/Program.cs
@@ -11,32 +11,19 @@ namespace calculo_de_sueldo
        
             static void Main(string[] args)
             {
-                //Uso de matrices. Calcular el suelo de una persona.
+            //secuencia fibonacci
+            int a = 0, b = 1, c = 1;
+            while (c<100)
+            {
+                Console.WriteLine("{0}+{1}={2}", a,b, c);
+                b = a;
+                a = c;
+                c = + a + b;
 
-                double[,] matriz = new double[,] {
-                {0.01,   487.6,  0, 0},
-                {487.61, 642.85, 10,17.48},
-                {642.86, 915.81, 10,32.70},
-                {915.82, 2058.67,20,60.00},
-                {2058.68,9999999,30,288.57},
-            };
-                Console.Write("Sueldo: ");
-                double sueldo = double.Parse(Console.ReadLine()),
-                    afp = sueldo * 7.25 / 100,
-                    isss = sueldo * 0.03;
-                sueldo -= afp;
+            }
+            
 
-                double isr = 0;
-                for (int i = 0; i < 5; i++)
-                {
-                    if (sueldo >= matriz[i, 0] && sueldo <= matriz[i, 1])
-                    {
-                        isr = (sueldo - (matriz[i, 0] - 0.01)) * matriz[i, 2] / 100 + matriz[i, 3];
-                    }
-                }
-                sueldo -= isss;
-                sueldo -= isr;
-                Console.WriteLine("Sueldo Neto: {0}, AFP: {1}, ISSS: {2}, ISR: {3}", Math.Round(sueldo, 2), afp, isss, Math.Round(isr, 2));
+              
 
                 Console.ReadLine();
             }
