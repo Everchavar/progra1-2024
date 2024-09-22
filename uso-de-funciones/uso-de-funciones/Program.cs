ac73b36d0a81113f89c2b3d7fb5109ffcf5cc170
Everchavar <everchavarria07@gmail.com>
2024-09-22 13:50:47 -0600

    uso de funciones

---
+++ uso-de-funciones/uso-de-funciones/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uso_de_funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uso de funciones...
+            Console.WriteLine("La suma de 10+5={0}", suma());
+            Console.ReadLine();
+        }
+        static int suma()
+        {
+            int respuesta = 10 + 5;
+            return respuesta;
+        }
+    }
+}
